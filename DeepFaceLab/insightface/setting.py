f75afa6b3da87b25d53f4bb1af056759415b3f09
lysx888 <270010678@qq.com>
2026-08-02 13:41:29 +0800

    Delete DeepFaceLab directory

--- DeepFaceLab/insightface/setting.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).parent.resolve()
-
-INSIGHTFACE_ROOT = os.environ.get('INSIGHTFACE_HOME', str(BASE_DIR))
-
-MODEL_ROOT = os.environ.get('INSIGHTFACE_MODEL_ROOT', str(BASE_DIR / "models"))
--- DeepFaceLab/insightface/utils/constant.py
+++
@@ -1,3 +0,0 @@
-
-DEFAULT_MP_NAME = 'antelopev2'
-
