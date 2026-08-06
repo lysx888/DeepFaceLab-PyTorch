0889baffed1d8b31f0ee5e6ef0072e3328d76428
lysx888 <270010678@qq.com>
2026-08-07 01:44:36 +0800

    Add files via upload

---
+++ faceswap/insightface/setting.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.resolve()
+
+INSIGHTFACE_ROOT = os.environ.get('INSIGHTFACE_HOME', str(BASE_DIR))
+
+MODEL_ROOT = os.environ.get('INSIGHTFACE_MODEL_ROOT', str(BASE_DIR.parent / "weights"))
