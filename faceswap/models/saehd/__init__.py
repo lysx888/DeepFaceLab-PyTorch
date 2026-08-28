e151a96ded1281a80c3e13e831dc706275b809fd
lysx888 <270010678@qq.com>
2026-08-29 04:57:29 +0800

    Delete faceswap/models directory

--- faceswap/models/saehd/__init__.py
+++
@@ -1,6 +0,0 @@
-from faceswap.models.saehd.saehd_arch import (
-    Downscale, DownscaleBlock, Upscale, ResidualBlock,
-    Encoder, Inter, Decoder,
-)
-from faceswap.models.saehd.discriminators import CodeDiscriminator, UNetPatchDiscriminator
-from faceswap.models.saehd.losses import dssim, style_loss, VGGFeatureExtractor
